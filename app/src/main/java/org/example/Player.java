c3821d83321632425509354302144bcac5bdbae4
HKR414 <e1c21068@oit.ac.jp>
2024-07-19 18:58:34 +0900

    SeMonsterGameクラス作成等

--- app/src/main/java/org/example/Player.java
+++ app/src/main/java/org/example/Player.java
@@ -1,27 +1,44 @@
 package org.example;
 
 public class Player {
-    String name;
-    Monster[] Deck = new Monster[8];
+  String name;
+  Monster[] Deck = new Monster[10];
+  int hp = 10;
+  int n = 0;
 
-    Player(){
-        this.name = "user";
-        MakeDeck();
-        System.out.println(toString());
+  Player() {
+    this.name = "user";
+    MakeDeck();
+    System.out.println(toString());
+  }
+
+  Player(String name) {
+    this.name = name;
+    MakeDeck();
+    System.out.println(toString());
+  }
+
+  void MakeDeck() {
+    for (int i = 0; i < 8; i++) {
+      Monster m = new Monster();
+      this.Deck[i] = m;
+      n++;
     }
+  }
 
-    void MakeDeck(){
-        for(int i = 0; i < 8; i++){
-            Monster m = new Monster();
-            this.Deck[i] = m;
-        }
+  public void drawMonsters() {
+    for (int i = n + 1; i < 10; n++) {
+      Monster m = new Monster();
+      this.Deck[i] = m;
+      n++;
     }
+  }
 
-    @Override
+  @Override
   public String toString() {
-    String result = "Deck:"+this.name + "\n";
-    for(int i = 0; i < 8; i++){
-        result = result + this.Deck[i].name + ":レア度[" + this.Deck[i].rare + "]\n";
+    String result = "Deck:" + this.name + " HP:" + this.hp + "\n";
+    for (int i = 0; i < 8; i++) {
+      result = result + this.Deck[i].name + ":レア度[" + this.Deck[i].rare + "]\n";
     }
 
     return result;
